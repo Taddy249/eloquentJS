0806ae237bf6af38afa0584462b641f55de9d589
Taddy <anhdt15@topica.edu.vn>
2018-03-05 00:05:49 +0700

    change the size of board

--- chap02_chess_broad.js
+++ chap02_chess_broad.js
@@ -1,7 +1,7 @@
-function chessBoard(){
+function chessBoard(n){
   let firstString = " # # # #";
   let secondString = "# # # # ";
-  for(let i = 1; i <= 8; i++){
+  for(let i = 1; i <= n; i++){
     if(i % 2 == 0){
       console.log(secondString);
     }else{
